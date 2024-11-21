317d876e66ccb6d43d2636fcab287e66e43943af
lbatres <lrbg_02@hotmail.com>
2024-11-21 17:54:06 -0600

    Actualización de configuración para Railway

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,18 +1,18 @@
 exports.config = {
     runner: 'local',
-    hostname: 'testapps.up.railway.app',
-    port: 4723,
-    path:'/wd/hub',
-    specs: ['./test/specs/**/*.js'],
+    hostname: 'testapps-production.up.railway.app', // URL pública de Railway
+    port: 443, // HTTPS
+    path: '/wd/hub',
+    specs: ['./test/specs/**/*.js'], // Ruta a tus archivos de prueba
     maxInstances: 1,
     capabilities: [
         {
             platformName: process.env.PLATFORM || 'Android',
             'appium:deviceName': process.env.PLATFORM === 'ios' ? 'iPhone 16' : 'emulator-5554',
-            'appium:platformVersion': process.env.PLATFORM === 'ios' ? '18.0' : undefined, 
+            'appium:platformVersion': process.env.PLATFORM === 'ios' ? '18.0' : undefined,
             'appium:automationName': process.env.PLATFORM === 'ios' ? 'XCUITest' : 'UiAutomator2',
             'appium:app': process.env.PLATFORM === 'ios'
-                ? 'https://appsreservamos2024r1.s3.us-east-2.amazonaws.com/gfa.app.zip' 
+                ? 'https://appsreservamos2024r1.s3.us-east-2.amazonaws.com/gfa.app.zip'
                 : 'https://appsreservamos2024r1.s3.us-east-2.amazonaws.com/gfa22112024.apk',
             'appium:newCommandTimeout': 3600,
             'appium:connectHardwareKeyboard': true
